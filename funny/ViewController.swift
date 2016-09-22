069742cf377260f3170c328a9b4e203a9c229030
Aaron King <norealm@gmail.com>
2016-09-22 11:31:04 +0800

    Initial Commit

---
+++ funny/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  funny
+//
+//  Created by Aaron King on 22/09/2016.
+//  Copyright © 2016 rightwon. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
